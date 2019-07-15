0a0651f4fde7b1d8837590a1e0b7a6565c054fed
akmphyu <akmphyu@gmail.com>
2019-07-15 17:46:28 +0630

    first commit

---
+++ PokeFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeFinder
+//
+//  Created by Aye Kaung Mya Phyu on 15/7/19.
+//  Copyright © 2019 Aye Kaung Mya Phyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
